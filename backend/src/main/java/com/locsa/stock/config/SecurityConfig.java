d827c6d9027fd2cd488e549c5cdc00569f794562
Hatim Abahri <hatimabahri9@gmail.com>
2026-03-27 11:47:10 +0100

    fix the cors

--- backend/src/main/java/com/locsa/stock/config/SecurityConfig.java
+++ backend/src/main/java/com/locsa/stock/config/SecurityConfig.java
@@ -54,7 +54,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("http://localhost*", "http://127.0.0.1*"));
+        configuration.setAllowedOriginPatterns(List.of(
+                "http://localhost*",
+                "http://127.0.0.1*",
+                "https://locsamaroc.tech",
+                "https://www.locsamaroc.tech"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
